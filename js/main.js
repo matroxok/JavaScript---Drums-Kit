00ac799b7424b28863a82fa283df77314fdd5dee
matroxok <matrox.biznesyt@gmail.com>
2024-09-13 22:18:39 +0200

    sound playing added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-const audio = document.querySelector('aud')
-
 const keyClick = e => {
-	console.log(e.keyCode)
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	// console.log(audio)
+
+	if (audio != null) {
+		audio.currentTime = 0
+		audio.play()
+	} else if (audio === null) {
+		console.warn('BLAD')
+		// dodac tutaj popup ze klawisz jest bledny
+	}
+}
+
+const keyAnimation = e => {
+	const animation = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+	console.log(animation)
+
+	animation.classList.add('test')
 }
 
 document.addEventListener('keydown', keyClick)
+document.addEventListener('keydown', keyAnimation)
